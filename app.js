70a759916ee4e9322b8aea3c74cf211e31a74f0d
Jan Horák <jan.342003@seznam.cz>
2020-08-26 12:02:15 +0200

    added header partial with navigation

--- app.js
+++ app.js
@@ -2,18 +2,25 @@ const ejs = require('ejs');
 
 const express = require('express');
 const app = express();
- 
+
 app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
 
 app.get('/', function (req, res) {
-  res.render("index");
+  res.render("pages/index");
 })
 
 app.get('/about', function (req, res) {
-  res.render("about");
+  res.render("pages/about");
 })
 
+app.get('/stock', function (req, res) {
+  res.render("pages/stock");
+})
+
+app.get('/contact', function (req, res) {
+  res.render("pages/contact");
+})
 
 app.listen(3000)
